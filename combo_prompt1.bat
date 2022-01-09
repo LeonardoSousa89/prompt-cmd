d11afb200f27bc7f152bda81c957e84d6b850702
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-01-09 12:45:23 -0300

    prompt-combo

--- combo_prompt1.bat
+++ combo_prompt1.bat
@@ -1,5 +1,6 @@
 shutdown -s -t 3600
 netstat -a -o
+echo 'torna uma pasta/arquivo invisivel/oculto ao usuário'
 attrib +s +h c:\users\leonardo\desktop\arquivo.txt
 echo 'combo prompt teste 1'
 ping -a 192.168.100.5
--- combo_prompt2.bat
+++ combo_prompt2.bat
@@ -1,3 +1,4 @@
+echo 'torna uma pasta/arquivo visivel ao usuário'
 attrib -s -h c:\users\leonardo\desktop\arquivo.txt
 shutdown -a
 echo 'combo prompt teste 2'
